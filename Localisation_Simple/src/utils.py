5c3c0c8b9203af3fb736ec73e840ddb558ac6347
Ronan Le Prat <ronan.leprat@laposte.net>
2026-02-14 15:26:55 +0100

    Generated instances + distance function

--- Localisation_Simple/src/utils.py
+++ Localisation_Simple/src/utils.py
@@ -2,7 +2,7 @@ import numpy as np
 import random
 import json
 
-def generate_manhattan(n, m, L=100, cost_range=range(10, 51), seed=1):
+def generate_points(n, m, L=100, cost_range=range(10, 51), seed=1):
     random.seed(seed)
     np.random.seed(seed)
 
@@ -40,16 +40,32 @@ def save_instance_json(coordinates_n, coordinates_m, f, L=None, seed=None):
         "f": list(map(int, f)),
         "meta": {
             "L": L,
-            "seed": seed,
-            "metric": "manhattan"
+            "seed": seed
         }
     }
 
     with open(filename, "w") as file:
         json.dump(data, file)
 
-def gen_and_save_manhattan(n, m, L=100, cost_range=range(10, 51), seed=1):
+def load_instance_json(filename):
+    with open(filename, "r") as file:
+        data = json.load(file)
 
-    coordinates_n, coordinates_m, f, L, seed = generate_manhattan(n, m, L, cost_range, seed)
+    return data
+
+def gen_and_save_instance(n, m, L=1000, cost_range=range(10, 51), seed=1):
+
+    coordinates_n, coordinates_m, f, L, seed = generate_points(n, m, L, cost_range, seed)
 
     save_instance_json(coordinates_n, coordinates_m, f, L, seed)
+
+def gen_and_save_multiple_instances(N, M, L=1000, cost_range=range(10, 51), seed=1):
+
+    for n in N:
+        for m in M:
+            if m <= n:
+                gen_and_save_instance(n, m, L, cost_range, seed)
+
+def distance(p1, p2, metric="manhattan"):
+
+    return abs(p1[0] - p2[0]) + abs(p1[1] - p1[1])
--- README.md
+++ README.md
@@ -9,3 +9,16 @@ Ce dépôt contient des implémentations & heuristiques pour des problèmes de l
 Les fichiers de données sont nommés de la manière suivante: 
 
 `{Nombre de clients}_{Nombre de sites}_{Côté de la grille}_{Seed}`
+
+Pour charger une instance, appeler la fonction `load_instance_json(filename)`.
+
+Cela retourne un dictionnaire contenant les clés suivantes:
+
+- `n`: Nombre de clients
+- `m`: Nombre de sites
+- `coordinates_n`: Coordonnées des clients dans la grille
+- `coordinates_m`: Coordonnées des sites dans la grille
+- `f`: Coûts fixes d'ouverture de site
+- `meta`: Meta-données - Côté le la grille `L` - Seed `seed`
+
+La fonction `distance` renvoie la distance de Manhattan (norme 1) entre deux points de la grille (donnés sous forme de liste).
